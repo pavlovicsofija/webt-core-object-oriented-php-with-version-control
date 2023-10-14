60e1158e7aa45cd0e39d3c8a59f49f18c5b331c8
oroz_lorena <0053@htl.rennweg.at>
2023-10-15 01:42:51 +0200

    youtube und vimeo videos displayed

--- index.php
+++ index.php
@@ -8,31 +8,75 @@ interface VideoInterface{
 abstract class Video implements VideoInterface{
     private $name;
     private $source;
-    private $code;
 
     /**
      * @param $name
      * @param $source
      * @param $code
      */
-    public function __construct($name, $source, $code)
+    public function __construct($name, $source)
     {
         $this->name = $name;
         $this->source = $source;
-        $this->code = $code;
     }
 
-    public function getName(): string {
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function getSource(): string {
+    public function getSource(): string
+    {
         return $this->source;
     }
 
-    public function getEmbedCode(): string {
-        return $this->getEmbedCode();
+    abstract public function getEmbedCode(): string;
+}
+
+class YouTubeVideo extends Video
+{
+    private $videoId;
+
+    public function __construct($name, $videoId)
+    {
+        parent::__construct($name, "YouTube");
+        $this->videoId = $videoId;
     }
 
+    public function getEmbedCode(): string {
+        return '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . $this->videoId . '"></iframe>';
+    }
 
 }
+
+class VimeoVideo extends Video
+{
+    private $videoId;
+
+    public function __construct($name, $videoId)
+    {
+        parent::__construct($name, "Vimeo");
+        $this->videoId = $videoId;
+    }
+
+    public function getEmbedCode(): string {
+        return '<iframe width="560" height="315" src="https://player.vimeo.com/video/' . $this->videoId . '"></iframe>';
+    }
+
+}
+$video = new VimeoVideo("keine Ahnung", "547868060");
+
+echo "Video Title: " . $video->getName() . "<br>";
+echo "Video Source: " . $video->getSource() . "<br>";
+
+echo "Video Embed Code: <br>";
+echo $video->getEmbedCode();
+
+$video = new YouTubeVideo("dog", "DzpmhfiBCO8");
+
+echo "Video Title: " . $video->getName() . "<br>";
+echo "Video Source: " . $video->getSource() . "<br>";
+
+echo "Video Embed Code: <br>";
+echo $video->getEmbedCode();
+
