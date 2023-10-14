15b155b5f0480349c2a67a890b02bd60c9ae9900
oroz_lorena <0053@htl.rennweg.at>
2023-10-14 23:07:46 +0200

    interface class basics

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
 <?php
-interface Video{
+interface VideoInterface{
     public function getName(): string;
     public function getSource(): string;
     public function getEmbedCode(): string;
+}
+
+abstract class Video implements VideoInterface{
+    private $name;
+    private $source;
+    private $code;
+
+    /**
+     * @param $name
+     * @param $source
+     * @param $code
+     */
+    public function __construct($name, $source, $code)
+    {
+        $this->name = $name;
+        $this->source = $source;
+        $this->code = $code;
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function getSource(): string {
+        return $this->source;
+    }
+
+    public function getEmbedCode(): string {
+        return $this->getEmbedCode();
+    }
+
+
 }
